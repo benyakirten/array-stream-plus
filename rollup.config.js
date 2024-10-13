db82975c9aab4e262e3692d9b040d5dba3f1f978
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-10-13 00:42:10 -0600

    Maybe create a tree-shakeable method of compilation [ci skip]

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,37 @@
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 
-export default {
-    input: "src/index.ts",
-    output: {
-        file: "dist/bundle.min.js",
-        format: "esm",
-        sourcemap: true,
+export default [
+    {
+        input: "src/stream/stream.ts",
+        output: {
+            file: "dist/stream.min.js",
+            format: "esm",
+            sourcemap: true,
+        },
+        plugins: [
+            typescript({
+                tsconfig: "./tsconfig.json",
+                declaration: true,
+                declarationDir: "dist/types",
+            }),
+            terser(),
+        ],
     },
-    plugins: [
-        typescript({
-            tsconfig: "./tsconfig.json",
-            declaration: true,
-            declarationDir: "dist/types",
-        }),
-        terser(),
-    ],
-};
+    {
+        input: "src/async-stream/async-stream.ts",
+        output: {
+            file: "dist/async-stream.min.js",
+            format: "esm",
+            sourcemap: true,
+        },
+        plugins: [
+            typescript({
+                tsconfig: "./tsconfig.json",
+                declaration: true,
+                declarationDir: "dist/types",
+            }),
+            terser(),
+        ],
+    },
+];
